eef00a95a28da99b7b833044180d22fad05b3873
rocher5200 <rocher5200@gmail.com>
2026-09-06 01:37:08 +0800

    Add protected dashboard data API

---
+++ api/dashboard-data.js
@@ -0,0 +1,13 @@
+import { getDashboardRows, getLastSync } from "./_lib/db.js";
+import { handleApiError, requireViewAuth, sendJson } from "./_lib/auth.js";
+
+export default async function handler(request, response) {
+  try {
+    if (request.method !== "GET") return sendJson(response, 405, { error: "Method not allowed" });
+    requireViewAuth(request);
+    const [rows, lastSync] = await Promise.all([getDashboardRows(), getLastSync()]);
+    sendJson(response, 200, { rows, lastSync });
+  } catch (error) {
+    handleApiError(response, error);
+  }
+}
